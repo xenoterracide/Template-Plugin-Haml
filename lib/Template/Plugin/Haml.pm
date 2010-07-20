97b813545acda8a3cb6b60a42846dff82e998c5c
Caleb Cushing <xenoterracide@gmail.com>
2010-07-20 01:45:00 -0400

    finish off documentation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Template/Plugin/Haml.pm
+++ lib/Template/Plugin/Haml.pm
@@ -30,6 +30,78 @@ sub filter {
 	<p>Hello World</p>
 
 =head1 DESCRIPTION
+
+A thin wrapper around L<Text::Haml> when you load the plugin it creates a
+filter called haml that you can use in a normal way. A full descritption of
+the Haml language can be found at
+L<<a href="http://haml-lang.com/">haml-lang.com</a>>. Haml is a terse and
+whitespace sensitive html, xhtml, and xml templating system. Compared to
+Template Toolkit however it is relatively limited in what you can do. It by
+itself contains no includes, conditionals, or loop constructs. So I've
+combined it here with TT to make writing those kinds of templates with even
+less code easier.
+
+=head2 EXAMPLE
+
+=head3 input template
+B<wrapper.tt>
+
+	!!! 5
+	%html
+	[% content %]
+
+B<hello.tt>
+
+	[%- message='Hello World' %]
+	[%- USE Haml -%]
+	[%- WRAPPER wrapper.tt | haml -%]
+	[%- FILTER haml -%]
+	 %head
+	  %meta{:charset => "utf-8"}
+	  %title hello
+	 %body
+	  %p [% message %]
+	  %ul
+	  [%- total=0; WHILE total < 5 %]
+	   %li [% total=total+1 %][% total %]
+	  [%- END -%]
+	[%- END -%]
+
+I'd like to draw some attention to the while loop here you have to have the
+-'s in just the right spots to make it work because of Haml's whitespace
+sensitivity.
+
+It's also important to note that <tags> will be </closed> in the templates
+they're in. If you used PROCESS instead of WRAPPER above your template would
+be output like
+
+	...
+	<html></html>
+	 <head>
+	...
+
+instead of...
+
+=head3 Output
+
+	<!DOCTYPE html>
+	<html>
+	 <head>
+	  <meta charset='utf-8' />
+	  <title>hello</title>
+	 </head>
+	 <body>
+	  <p>hello world</p>
+	  <ul>
+	   <li>1</li>
+	   <li>2</li>
+	   <li>3</li>
+	   <li>4</li>
+	   <li>5</li>
+	  </ul>
+	 </body>
+	</html>
+
 =head2 Methods
 
 =over
