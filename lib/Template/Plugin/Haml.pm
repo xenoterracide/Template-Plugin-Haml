4cc5ab7532b36c858d2de1e25d389ed6d1a92639
Caleb Cushing <xenoterracide@gmail.com>
2010-07-19 22:39:59 -0400

    add working implementation
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Template/Plugin/Haml.pm
+++ lib/Template/Plugin/Haml.pm
@@ -2,4 +2,20 @@ use strict;
 use warnings;
 package Template::Plugin::Haml;
 
+use parent 'Template::Plugin::Filter';
+use Text::Haml;
+
+sub init {
+	my $self = shift;
+	$self->{_DYNAMIC} = 1;
+	$self->install_filter( $self->{_ARGS}->[0] || 'haml');
+	$self;
+}
+
+sub filter {
+	my ( $self, $text, $args, $config ) = @_;
+
+	my $haml = Text::Haml->new;
+	return $haml->render($text);
+}
 1;
