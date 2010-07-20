ce6e0885ef2a3d66d928762768fe77c85533508b
Caleb Cushing <xenoterracide@gmail.com>
2010-07-19 23:03:15 -0400

    Build results of 1130660 (on master)

--- lib/Template/Plugin/Haml.pm
+++ lib/Template/Plugin/Haml.pm
@@ -1,6 +1,9 @@
 use strict;
 use warnings;
 package Template::Plugin::Haml;
+BEGIN {
+  $Template::Plugin::Haml::VERSION = '0.1.0';
+}
 
 use parent 'Template::Plugin::Filter';
 use Text::Haml;
@@ -20,3 +23,29 @@ sub filter {
 }
 1;
 # ABSTACT: HAML plugin for Template Toolkit
+
+__END__
+=pod
+
+=head1 NAME
+
+Template::Plugin::Haml
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 AUTHOR
+
+Caleb Cushing <xenoterracide@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2010 by Caleb Cushing.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
+=cut
+
